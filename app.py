49f64b6ba7ed6993a624c1a96395da7826282ff3
Yomanbest <106496623+Yomanbest67@users.noreply.github.com>
2025-11-04 17:32:55 +0000

    Fix server starting locally

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def data():
     return jsonify({'temperature': dht22.getTemperature(), 'humidity': dht22.getHumidity(), 'lux': ltr390.getLux(), 'uvi': ltr390.getUvi()}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
