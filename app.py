212c5106aff3f4cee68492b1274490fa522ca999
Yomanbest <106496623+Yomanbest67@users.noreply.github.com>
2025-11-04 17:37:28 +0000

    Json non serializable fix

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ def index():
 
 @app.route('/data')
 def data():
-    return jsonify({'temperature': dht22.getTemperature(), 'humidity': dht22.getHumidity(), 'lux': ltr390.getLux(), 'uvi': ltr390.getUvi()}), 200
+    temperature = dht22.getTemperature()
+    humidity = dht22.getHumidity()
+    lux = ltr390.getLux()
+    uvi = ltr390.getUvi()
+
+    return jsonify({
+        'temperature': temperature, #
+        'humidity': humidity, 
+        'lux': lux, 
+        'uvi': uvi
+    }), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
